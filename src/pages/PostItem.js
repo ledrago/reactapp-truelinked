0a3e522d478ccbe743de6f863fbfbd65dba7d072
ledrago <jeromedrago@gmail.com>
2020-10-06 10:41:04 +0200

    second commit

--- src/pages/PostItem.js
+++ src/pages/PostItem.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
+import PostService from '../services/PostService';
+import UserService from '../services/UserService'
 
 const PostItem = () => {
+
+    const location = useLocation();
+    const [postItem, setPostItem] = useState(null);
+
+    useEffect(() => {
+
+        const currentUrl = (location.pathname);
+        const urlPostId = currentUrl.split('/')[2];
+        getPostAndUserData(urlPostId);
+    }, [location])
+
+    const getPostAndUserData = (urlPostId) => {
+        PostService.getPost(urlPostId).then(res => {
+            setPostItem(res.data);
+            console.log('res : ', res.data)
+        }).then((res) => {
+            console.log('pipou : ', res);
+        })
+    }
+
     return (
         <p>Post Item</p>
     )
