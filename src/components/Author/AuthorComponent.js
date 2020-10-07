f8751ab7fc68bf9a82e1c2fc1537868886b9ce2d
ledrago <jeromedrago@gmail.com>
2020-10-07 10:19:25 +0200

    components refactoring

--- src/components/Author/AuthorComponent.js
+++ src/components/Author/AuthorComponent.js
@@ -2,15 +2,13 @@ import React from "react";
 import authorStyle from "./AuthorComponent.module.scss";
 import meImg from './jerome-drago.jpg';
 
-const AuthorComponent = ({ userData }) => {
-    return (
-        <div className={authorStyle.container}>
-            <img alt="author" src={meImg} />
-            <p className={authorStyle.name}>{userData?.name}</p>
-            <p className={authorStyle.mailphone}>{userData?.email} - {userData?.phone}</p>
-            <p className={authorStyle.website}>{userData?.website}</p>
-        </div>
-    )
-};
+const AuthorComponent = ({ userData }) => (
+    <div className={authorStyle.container}>
+        <img alt="author" src={meImg} />
+        <p className={authorStyle.name}>{userData?.name}</p>
+        <p className={authorStyle.mailphone}>{userData?.email} - {userData?.phone}</p>
+        <p className={authorStyle.website}>{userData?.website}</p>
+    </div>
+);
 
 export default AuthorComponent;
--- src/components/Layout/LayoutComponent.js
+++ src/components/Layout/LayoutComponent.js
@@ -5,16 +5,15 @@ import Footer from "../Footer/FooterComponent";
 
 import layoutStyle from "./LayoutComponent.module.scss";
 
-const Layout = (props) => {
-  return (
-    <div className={layoutStyle.container}>
-      <div className={layoutStyle.content}>
-        <Header />
-        <div className={layoutStyle.main}>{props.children}</div>
-      </div>
-      <Footer />
+const Layout = (props) => (
+  <div className={layoutStyle.container}>
+    <div className={layoutStyle.content}>
+      <Header />
+      <div className={layoutStyle.main}>{props.children}</div>
     </div>
-  );
-};
+    <Footer />
+  </div>
+);
+
 
 export default Layout;
